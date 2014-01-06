9b1f49841140bbbbc500d042306565891d6c2d31
Thomas Svensen <thomas.svensen@bekk.no>
2014-01-06 11:04:29 +0100

    Responding with username as a start

--- server.js
+++ server.js
@@ -16,10 +16,39 @@ app.get('/', function(req, res) {
       if(error) {
         console.log("an error has occured. keep calm and carry on.");
       }
-      res.json(body);
+      res.json(response);
     });
+});
+
+app.get('/messages', function(req, res) {
+  request.get({
+    url: demo_url,
+    json: true,
+    headers: {
+            'User-Agent': 'request'
+                }
+    }, function(error, response, body) {
+      if(error) {
+        console.log("an error has occured. keep calm and carry on.");
+      }
+      res.json(process.env.username);
+    });
+});
 
 
+app.get('/message/{id}', function(req, res) {
+  request.get({
+    url: demo_url,
+    json: true,
+    headers: {
+            'User-Agent': 'request'
+                }
+    }, function(error, response, body) {
+      if(error) {
+        console.log("an error has occured. keep calm and carry on.");
+      }
+      // Returner data fra API
+    });
 });
 
 
